177bea3965faa2460ed9dd2cb2f6c57ef0056b3c
Patchanonss <patchanonssc@gmail.com>
2025-05-21 00:38:18 +0700

    test comm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  nitro: {
+    preset: "static",
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  // css: ["~/assets/css/main.css"],
   modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/tailwindcss"],
   experimental: {
     payloadExtraction: false,
